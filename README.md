268590d09ecae8817e8bf0417d7c21d10373635d
Adan Scotney <adan.scotney@gmail.com>
2019-05-08 15:41:24 +0100

    Test code to prove crt0 is working, update readme with instructions for running via JTAG, openocd config

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # orcus
 Low level library for GP2X
+
+To launch an elf executable via JTAG...
+
+sudo openocd -f gp2x-openocd.cfg
+
+$DEVKITARM/bin/arm-none-eabi-gdb
+exec-file YOUR.elf
+target remote localhost:3333
+load
+cont
+
+After recompiling, just run ctrl+c to kill the process within gdb then 'load' and 'cont' again.
--- source/main.c
+++ source/main.c
@@ -1,7 +1,22 @@
 #include <gp2xregs.h>
 
-int main() {
-  *_irq = 0xdeadbeef;
+void init() {
+  // a little test to see if we can even get the thing to execute, if we can read 0xdeadbeef
+  // from 0x2000 in memory via jtag, the binary is running
+  volatile r32 test = (r32) 0x2000;
+  *test = 0xdeadbeea;
 
+  unsigned int test2_data = 0x123;
+  volatile r32 test2 = (r32) 0x3000;
+  *test2 = test2_data;
+
+  unsigned int test3_data;
+  volatile r32 test3 = (r32) 0x4000;
+  *test3 = test3_data;
+  
+}
+
+int main() {
+  init();
   while(1);
 }
